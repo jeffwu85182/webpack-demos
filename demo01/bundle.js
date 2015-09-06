bddcb142e6a5ac0cd9bc253d8f40bfec18d1b7de
Jeff Wu <a60985182@hotmail.com>
2015-09-06 10:32:24 +0800

    test for webpack bundle.js

---
+++ demo01/bundle.js
@@ -0,0 +1,5 @@
+!function(r){function e(t){if(o[t])return o[t].exports;var n=o[t]={exports:{},id:t,loaded:!1};return r[t].call(n.exports,n,n.exports,e),n.loaded=!0,n.exports}var o={};return e.m=r,e.c=o,e.p="",e(0)}([/*!*****************!*\
+  !*** ./main.js ***!
+  \*****************/
+function(r,e){document.write("<h1>Hello World</h1>")}]);
+//# sourceMappingURL=bundle.js.map
